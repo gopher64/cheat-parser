aa7af3571036e30d9cb7aa5ad829e5e4410126c9
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-07-04 20:22:57 +0200

    Add enhancements

--- cheat-parser.go
+++ cheat-parser.go
@@ -35,57 +35,61 @@ func main() {
 		log.Panic(err)
 	}
 
-	basePath := "Config/Cheats"
-	items, err := fs.ReadDir(basePath)
-	if err != nil {
-		log.Panic(err)
-	}
-
 	cheatDB := map[string]map[string]Cheat{}
-	currentGame := ""
-	currentCheat := ""
-	for _, item := range items {
-		file, err := fs.Open(filepath.Join(basePath, item.Name()))
+
+	paths := []string{"Config/Cheats", "Config/Enhancements"}
+	for _, basePath := range paths {
+		items, err := fs.ReadDir(basePath)
 		if err != nil {
 			log.Panic(err)
 		}
-		defer file.Close() //nolint:errcheck
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			game := cheatDB[currentGame]
-			cheat := game[currentCheat]
-			if strings.HasPrefix(scanner.Text(), "[") {
-				currentGame = strings.Trim(scanner.Text(), "[]")
-				cheatDB[currentGame] = map[string]Cheat{}
-			} else if strings.HasPrefix(scanner.Text(), "Name=") {
-				// do nothing
-			} else if strings.HasPrefix(scanner.Text(), "$") {
-				currentCheat = strings.TrimPrefix(scanner.Text(), "$")
-				game[currentCheat] = Cheat{}
-				cheatDB[currentGame] = game
-			} else if strings.HasPrefix(scanner.Text(), "Note=") {
-				cheat.Note = strings.TrimPrefix(scanner.Text(), "Note=")
-				game[currentCheat] = cheat
-			} else if scanner.Text() == "" {
-				// do nothing
-			} else if strings.Contains(scanner.Text(), "?") && cheat.Options == nil {
-				cheat.Options = map[string]string{}
-				cheat.Data = append(cheat.Data, scanner.Text())
-				game[currentCheat] = cheat
-			} else if cheat.Options != nil && len(strings.Split(scanner.Text(), " ")[0]) < 8 {
-				cheat.Options[strings.Join(strings.Split(scanner.Text(), " ")[1:], " ")] = strings.Split(scanner.Text(), " ")[0]
-				game[currentCheat] = cheat
-			} else if isHex(scanner.Text()) {
-				cheat.Data = append(cheat.Data, scanner.Text())
-				game[currentCheat] = cheat
-			} else {
-				delete(game, currentCheat)
-				log.Printf("Unknown line in cheat file %s, %s: %s\n", filepath.Join(basePath, item.Name()), currentCheat, scanner.Text())
+		for _, item := range items {
+			currentGame := ""
+			currentCheat := ""
+			file, err := fs.Open(filepath.Join(basePath, item.Name()))
+			if err != nil {
+				log.Panic(err)
 			}
-		}
+			defer file.Close() //nolint:errcheck
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				game := cheatDB[currentGame]
+				cheat := game[currentCheat]
+				if strings.HasPrefix(scanner.Text(), "[") {
+					currentGame = strings.Trim(scanner.Text(), "[]")
+					if cheatDB[currentGame] == nil {
+						cheatDB[currentGame] = map[string]Cheat{}
+					}
+				} else if strings.HasPrefix(scanner.Text(), "Name=") {
+					// do nothing
+				} else if strings.HasPrefix(scanner.Text(), "$") {
+					currentCheat = strings.TrimPrefix(scanner.Text(), "$")
+					game[currentCheat] = Cheat{}
+					cheatDB[currentGame] = game
+				} else if strings.HasPrefix(scanner.Text(), "Note=") {
+					cheat.Note = strings.TrimPrefix(scanner.Text(), "Note=")
+					game[currentCheat] = cheat
+				} else if scanner.Text() == "" {
+					// do nothing
+				} else if strings.Contains(scanner.Text(), "?") && cheat.Options == nil {
+					cheat.Options = map[string]string{}
+					cheat.Data = append(cheat.Data, scanner.Text())
+					game[currentCheat] = cheat
+				} else if cheat.Options != nil && len(strings.Split(scanner.Text(), " ")[0]) < 8 {
+					cheat.Options[strings.Join(strings.Split(scanner.Text(), " ")[1:], " ")] = strings.Split(scanner.Text(), " ")[0]
+					game[currentCheat] = cheat
+				} else if isHex(scanner.Text()) {
+					cheat.Data = append(cheat.Data, scanner.Text())
+					game[currentCheat] = cheat
+				} else {
+					delete(game, currentCheat)
+					log.Printf("Unknown line in cheat file %s, %s: %s\n", filepath.Join(basePath, item.Name()), currentCheat, scanner.Text())
+				}
+			}
 
-		if err := scanner.Err(); err != nil {
-			log.Panic(err)
+			if err := scanner.Err(); err != nil {
+				log.Panic(err)
+			}
 		}
 	}
 
