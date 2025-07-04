94ac03eddbed15490d872869befb9fd8ff11a73b
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2025-07-04 21:59:52 +0200

    more

--- cheat-parser.go
+++ cheat-parser.go
@@ -53,8 +53,6 @@ func main() {
 			defer file.Close() //nolint:errcheck
 			scanner := bufio.NewScanner(file)
 			for scanner.Scan() {
-				game := cheatDB[currentGame]
-				cheat := game[currentCheat]
 				if strings.HasPrefix(scanner.Text(), "[") {
 					currentGame = strings.Trim(scanner.Text(), "[]")
 					if cheatDB[currentGame] == nil {
@@ -64,26 +62,33 @@ func main() {
 					// do nothing
 				} else if strings.HasPrefix(scanner.Text(), "$") {
 					currentCheat = strings.TrimPrefix(scanner.Text(), "$")
-					game[currentCheat] = Cheat{}
-					cheatDB[currentGame] = game
-				} else if strings.HasPrefix(scanner.Text(), "Note=") {
-					cheat.Note = strings.TrimPrefix(scanner.Text(), "Note=")
-					game[currentCheat] = cheat
+					cheatDB[currentGame][currentCheat] = Cheat{}
+				} else if strings.HasPrefix(scanner.Text(), "Note=") && currentCheat != "" {
+					current := cheatDB[currentGame][currentCheat]
+					current.Note = strings.TrimPrefix(scanner.Text(), "Note=")
+					cheatDB[currentGame][currentCheat] = current
 				} else if scanner.Text() == "" {
 					// do nothing
-				} else if strings.Contains(scanner.Text(), "?") && cheat.Options == nil {
-					cheat.Options = map[string]string{}
-					cheat.Data = append(cheat.Data, scanner.Text())
-					game[currentCheat] = cheat
-				} else if cheat.Options != nil && len(strings.Split(scanner.Text(), " ")[0]) < 8 {
-					cheat.Options[strings.Join(strings.Split(scanner.Text(), " ")[1:], " ")] = strings.Split(scanner.Text(), " ")[0]
-					game[currentCheat] = cheat
-				} else if isHex(scanner.Text()) {
-					cheat.Data = append(cheat.Data, scanner.Text())
-					game[currentCheat] = cheat
-				} else {
-					delete(game, currentCheat)
+				} else if strings.Contains(scanner.Text(), "?") && currentCheat != "" && cheatDB[currentGame][currentCheat].Options == nil {
+					current := cheatDB[currentGame][currentCheat]
+					current.Options = map[string]string{}
+					current.Data = append(cheatDB[currentGame][currentCheat].Data, scanner.Text())
+					cheatDB[currentGame][currentCheat] = current
+				} else if currentCheat != "" && cheatDB[currentGame][currentCheat].Options != nil && len(strings.Split(scanner.Text(), " ")[0]) < 8 {
+					cheatDB[currentGame][currentCheat].Options[strings.Join(strings.Split(scanner.Text(), " ")[1:], " ")] = strings.Split(scanner.Text(), " ")[0]
+				} else if isHex(scanner.Text()) && currentCheat != "" {
+					current := cheatDB[currentGame][currentCheat]
+					current.Data = append(cheatDB[currentGame][currentCheat].Data, scanner.Text())
+					cheatDB[currentGame][currentCheat] = current
+				} else if currentCheat != "" && (strings.HasPrefix(scanner.Text(), "OnByDefault=1") || strings.HasPrefix(scanner.Text(), "PluginList=")) {
+					// PJ64 specific, ignore
+					delete(cheatDB[currentGame], currentCheat)
+					log.Printf("Ignoring line in cheat file %s, %s: %s\n", filepath.Join(basePath, item.Name()), currentCheat, scanner.Text())
+					currentCheat = ""
+				} else if currentCheat != "" {
+					delete(cheatDB[currentGame], currentCheat)
 					log.Printf("Unknown line in cheat file %s, %s: %s\n", filepath.Join(basePath, item.Name()), currentCheat, scanner.Text())
+					currentCheat = ""
 				}
 			}
 
